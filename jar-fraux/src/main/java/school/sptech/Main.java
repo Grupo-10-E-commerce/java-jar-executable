8bd36c7fe989773fae60aeaba164a1b5c1366bf2
flaviachristine <flavia.pereira@sptech.school>
2025-10-24 16:20:32 -0300

    s3 provider adicionado

--- jar-fraux/src/main/java/school/sptech/Main.java
+++ jar-fraux/src/main/java/school/sptech/Main.java
@@ -1,9 +1,19 @@
 package school.sptech;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import school.sptech.client.S3Provider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.core.sync.ResponseTransformer;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,9 +36,11 @@ public class Main {
             String TransactionType = comprasList.get(i).getTipo_transacao();
             String Location = comprasList.get(i).getCidade();
             Integer IsFraud = comprasList.get(i).getFraude();
-            connection.update("INSERT INTO compra VALUES (?,?,?,?,?,?,?)", TransactionID, TransactionDate, Amount, MerchantID, TransactionType, Location, IsFraud);
-            //connection.update("INSERT INTO logs VALUES (Default, ?, Default)", "Compra : " + TransactionID + " Inserida com sucesso");
+            // connection.update("INSERT INTO compra VALUES (?,?,?,?,?,?,?)", TransactionID, TransactionDate, Amount, MerchantID, TransactionType, Location, IsFraud);
+
+            // connection.update("INSERT INTO logs VALUES (Default, ?, Default)", "Compra : " + TransactionID + " Inserida com sucesso");
         }
+
 
     }
 }
